cdc57352670f430def84d269dded528317a4b88b
imoyakin <imoyaki@outlook.com>
2019-07-18 15:34:43 +0800

    change to channel

--- src/handle/mod.rs
+++ src/handle/mod.rs
@@ -5,10 +5,10 @@ use std::rc::Rc;
 use super::config;
 
 pub fn handle(update:telegram_bot::Update, api:Rc<RefCell<telegram_bot::Api>>) -> Result<(),telegram_bot::Error>{
-    if let UpdateKind::Message(message) = update.kind {
+    if let UpdateKind::ChannelPost(message) = update.kind {
         if let MessageKind::Text {ref data, ..} = message.kind {
                 // Print received text message to stdout.
-                println!("<{}>: {}", &message.from.first_name, data);
+                //println!("<{}>: {}", &message.from.first_name, data);
 
                 let mut matches:Vec<_> = config::RE.matches(data).into_iter().collect::<Vec<usize>>();
                 let mut ret = "".to_string();  
@@ -36,7 +36,7 @@ pub fn handle(update:telegram_bot::Update, api:Rc<RefCell<telegram_bot::Api>>) -
                 // }
                 // Answer message with "Hi".
                 api.borrow_mut().spawn(message.text_reply(
-                    format!("{}:{}", &message.from.first_name, ret)
+                    format!("{}", ret)
                 ));
         }
     }
