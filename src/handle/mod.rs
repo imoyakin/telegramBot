85b03e3d08c71a31b082d09df901c0064e3f250e
imoyakin <imoyaki@outlook.com>
2019-07-18 17:22:09 +0800

    update

--- src/handle/mod.rs
+++ src/handle/mod.rs
@@ -1,21 +1,27 @@
-use telegram_bot::{CanReplySendMessage, MessageKind, UpdateKind};
 use std::cell::RefCell;
 use std::rc::Rc;
+use telegram_bot::{CanReplySendMessage, MessageKind, UpdateKind};
 
 use super::config;
 
-pub fn handle(update:telegram_bot::Update, api:Rc<RefCell<telegram_bot::Api>>) -> Result<(),telegram_bot::Error>{
-    if let UpdateKind::ChannelPost(message) = update.kind {
-        if let MessageKind::Text {ref data, ..} = message.kind {
+pub fn handle(
+    update: telegram_bot::Update,
+    api: Rc<RefCell<telegram_bot::Api>>,
+) -> Result<(), telegram_bot::Error> {
+    match update.kind {
+        UpdateKind::ChannelPost(message) => {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                println!("{}", data);
                 // Print received text message to stdout.
                 //println!("<{}>: {}", &message.from.first_name, data);
 
-                let mut matches:Vec<_> = config::RE.matches(data).into_iter().collect::<Vec<usize>>();
-                let mut ret = "".to_string();  
+                let mut matches: Vec<_> =
+                    config::RE.matches(data).into_iter().collect::<Vec<usize>>();
+                let mut ret = "".to_string();
 
-                if  matches.len() == 0 {
-                    return Ok(())
-                }      
+                if matches.len() == 0 {
+                    return Ok(());
+                }
 
                 matches.reserve(matches.len());
 
@@ -27,7 +33,6 @@ pub fn handle(update:telegram_bot::Update, api:Rc<RefCell<telegram_bot::Api>>) -
                     }
                     i = i + 1;
                 }
-                
                 // for i in &config::CONF.auto_resp {
                 //     match caps.name(&i.key) {
                 //         Some(_)=>ret += &i.value[0].clone(),
@@ -35,11 +40,47 @@ pub fn handle(update:telegram_bot::Update, api:Rc<RefCell<telegram_bot::Api>>) -
                 //     }
                 // }
                 // Answer message with "Hi".
-                api.borrow_mut().spawn(message.text_reply(
-                    format!("{}", ret)
-                ));
+                api.borrow_mut()
+                    .spawn(message.text_reply(format!("{}", ret)));
+            }
         }
-    }
+        UpdateKind::Message(message) => {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                println!("{}", data);
+                // Print received text message to stdout.
+                //println!("<{}>: {}", &message.from.first_name, data);
+
+                let mut matches: Vec<_> =
+                    config::RE.matches(data).into_iter().collect::<Vec<usize>>();
+                let mut ret = "".to_string();
+
+                if matches.len() == 0 {
+                    return Ok(());
+                }
+
+                matches.reserve(matches.len());
+
+                let mut i = 0usize;
+                for j in &config::CONF.auto_resp {
+                    if &i == matches.get(0usize).unwrap_or(&0usize) {
+                        ret += &j.value[0];
+                        matches.pop();
+                    }
+                    i = i + 1;
+                }
+                // for i in &config::CONF.auto_resp {
+                //     match caps.name(&i.key) {
+                //         Some(_)=>ret += &i.value[0].clone(),
+                //         _ => ret += "what are you saying",
+                //     }
+                // }
+                // Answer message with "Hi".
+                api.borrow_mut()
+                    .spawn(message.text_reply(format!("{}", ret)));
+            }
+        }
+        _ => {}
+    };
 
     Ok(())
 }
